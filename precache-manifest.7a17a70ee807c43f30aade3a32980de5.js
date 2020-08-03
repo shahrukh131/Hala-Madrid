63fb665dcd8cd8fc71c9220d84ad3de342c8c5a4
Shahrukh-akib <Shahrukhakib@gmail.com>
2020-08-03 12:26:11 +0600

    Updates

---
+++ precache-manifest.7a17a70ee807c43f30aade3a32980de5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c229d796a42218babd9f3a089d778372",
+    "url": "/Hala-Madrid/index.html"
+  },
+  {
+    "revision": "4e126c4e737affd7f1fc",
+    "url": "/Hala-Madrid/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "f9577983baa1378adcf2",
+    "url": "/Hala-Madrid/static/css/main.40bbff45.chunk.css"
+  },
+  {
+    "revision": "4e126c4e737affd7f1fc",
+    "url": "/Hala-Madrid/static/js/2.27ead5eb.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Hala-Madrid/static/js/2.27ead5eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9577983baa1378adcf2",
+    "url": "/Hala-Madrid/static/js/main.49549d80.chunk.js"
+  },
+  {
+    "revision": "6f6c35b6d17f5351bedf",
+    "url": "/Hala-Madrid/static/js/runtime-main.4484c361.js"
+  },
+  {
+    "revision": "627ca6ce5079ae6d77f064fba9f8987f",
+    "url": "/Hala-Madrid/static/media/cup.627ca6ce.png"
+  },
+  {
+    "revision": "45d3b0686012830120829975d337a698",
+    "url": "/Hala-Madrid/static/media/jersey1.45d3b068.jpg"
+  },
+  {
+    "revision": "3070823b03ce629015a12037e25bb6ea",
+    "url": "/Hala-Madrid/static/media/jersey2.3070823b.jpg"
+  },
+  {
+    "revision": "e0e579f449f818abc1c662405e517b8a",
+    "url": "/Hala-Madrid/static/media/jersey3.e0e579f4.jpg"
+  },
+  {
+    "revision": "e31b006dd03d0bb62132540c695cb4c5",
+    "url": "/Hala-Madrid/static/media/jersey4.e31b006d.jpg"
+  },
+  {
+    "revision": "d6b503c16fc29443c46da3bd4baca6ca",
+    "url": "/Hala-Madrid/static/media/jersey5.d6b503c1.jpg"
+  },
+  {
+    "revision": "b3fee018849dab22ee91264bb4680223",
+    "url": "/Hala-Madrid/static/media/jersey6.b3fee018.jpg"
+  },
+  {
+    "revision": "6167950c0371dbd9ed4ad387b417ce6c",
+    "url": "/Hala-Madrid/static/media/madrid.6167950c.gif"
+  },
+  {
+    "revision": "6ca5a38a40fe4a6fe897dce1aebac78d",
+    "url": "/Hala-Madrid/static/media/madrid.6ca5a38a.png"
+  }
+]);
